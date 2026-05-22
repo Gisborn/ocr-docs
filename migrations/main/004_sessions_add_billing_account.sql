7f77b3289d9eca69931e1715efca8c83b088aaa8
meaningful.trout <meaningful.trout.iaop@mask.me>
2026-05-22 16:12:49 +0200

    fix: add goose directives to migration 004

--- migrations/main/004_sessions_add_billing_account.sql
+++ migrations/main/004_sessions_add_billing_account.sql
@@ -1,8 +1,11 @@
--- Добавляем billing_account_id в sessions
+-- +goose Up
+-- +goose StatementBegin
+
 ALTER TABLE sessions ADD COLUMN billing_account_id BIGINT DEFAULT 0;
 
--- Обновляем существующие сессии из organizations
 UPDATE sessions s
 SET billing_account_id = COALESCE(o.billing_account_id, 0)
 FROM organizations o
 WHERE s.org_id = o.id;
+
+-- +goose StatementEnd
